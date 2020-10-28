14e8352d25822953a9c96503d1f9745b2bf7690c
ttaroo1217 <69450720+ttaroo1217@users.noreply.github.com>
2020-10-28 16:47:37 +0900

    initial commit

---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,61 @@
+class PrototypesController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_prototype, only: [:edit, :show]
+  before_action :move_to_index, only: [:edit]
+
+  def index
+    @prototypes = Prototype.all
+    
+  end
+
+  def new
+    @prototype = Prototype.new
+  end
+
+  def create
+    @prototype = Prototype.create(prototypes_params)
+    if @prototype.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @comment = Comment.new
+    @comments = @prototype.comments.includes(:user)
+  end
+
+  def edit
+  end
+
+  def update
+    prototype = Prototype.find(params[:id])
+    if prototype.update(prototypes_params)
+      redirect_to prototype_path
+    else
+      render :edit
+    end  
+  end
+
+  def destroy
+    prototype = Prototype.find(params[:id])
+    prototype.destroy
+    redirect_to root_path
+  end
+
+  private
+  def prototypes_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def move_to_index
+    unless user_signed_in? && @prototype.user_id == current_user.id
+      redirect_to action: :index
+    end
+  end
+end
